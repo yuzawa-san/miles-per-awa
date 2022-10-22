c770accf94507fd45303ad51b27b6068c57ac7e8
James Yuzawa <jtyuzawa@gmail.com>
2022-10-21 21:18:44 -0400

    get spring working

---
+++ src/main/java/com/jyuzawa/miles_per_awa/MilesPerAwa.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2022 James Yuzawa (https://www.jyuzawa.com/)
+ * All rights reserved. Licensed under the MIT License.
+ */
+package com.jyuzawa.miles_per_awa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilesPerAwa {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilesPerAwa.class, args);
+    }
+}
